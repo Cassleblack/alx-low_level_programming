41e772a0b390d43f47f15c301482da04adf0819e
olowe christian <olowechristian@gmail.com>
2023-04-09 07:14:14 -0700

    static libraries

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -5,7 +5,7 @@
  * @s: string
  * Return: lenght
  */
-int_strlen(char *s)
+int _strlen(char *s)
 {
 	int longi = 0;
 
--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -6,7 +6,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void _puts(char *str)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
