46132b369958ca202fd4af931abe4cf6dc3e0569
olowe christian <olowechristian@gmail.com>
2024-01-16 07:08:51 +0800

    double linked list

--- 0x17-doubly_linked_lists/2-main.c
+++ 0x17-doubly_linked_lists/2-main.c
@@ -10,10 +10,10 @@
  */
 int main(void)
 {
-    dlistint_t *head;
-    
-    head = NULL;
-    add_dnodeint(&head, 0);
+	dlistint_t *head;
+
+	head = NULL;
+add_dnodeint(&head, 0);
     add_dnodeint(&head, 1);
     add_dnodeint(&head, 2);
     add_dnodeint(&head, 3);
