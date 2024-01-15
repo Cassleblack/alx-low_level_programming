5b79c8240968c1957d7ed4c068e73d564cb95195
cassle black <olowechristian@gmail.com>
2024-01-15 15:34:44 -0800

    Dynamic libraries

--- 0x18-dynamic_libraries/Myfunctions/3-puts.c
+++ 0x18-dynamic_libraries/Myfunctions/3-puts.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /*
- * _puts: writes a string
+ * _puts: writes a string writes a string writes a string
  * str: the giving string
  */
 void _puts(char *str)
 {
-	puts (str);
+	puts(str);
 }
